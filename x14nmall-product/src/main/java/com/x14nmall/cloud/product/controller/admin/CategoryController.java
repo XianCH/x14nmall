3711885aee13c758ea513851f0ff374312264904
“XianCH” <1160698903@qq.com>
2023-04-09 07:19:46 +0800

    .

---
+++ x14nmall-product/src/main/java/com/x14nmall/cloud/product/controller/admin/CategoryController.java
@@ -0,0 +1,10 @@
+package com.x14nmall.cloud.product.controller.admin;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminCategoryController")
+@RequestMapping("/category")
+public class CategoryController {
+}
---
+++ x14nmall-product/src/main/java/com/x14nmall/cloud/product/controller/admin/ProductController.java
@@ -0,0 +1,22 @@
+package com.x14nmall.cloud.product.controller.admin;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminProductController")
+@RequestMapping("/product")
+public class ProductController {
+
+    /**
+     * 添加商品
+     */
+
+    /**
+     * 修改商品
+     */
+
+    /**
+     *
+     */
+}
